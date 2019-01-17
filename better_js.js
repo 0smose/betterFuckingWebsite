95fae56cf622c11222f8048e95ac97236d775c45
0smose <robin.dercole@gmail.com>
2019-01-17 17:38:20 +0100

    le commit de betterFuckingWebsite

---
+++ better_js.js
@@ -0,0 +1,29 @@
+
+
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer(){
+	
+	if (confirm ("veux tu aller sur xxxxvidsxxx ???")){
+		window.open ("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); 
+		
+		}
+
+	}
+function myFunction() {
+  document.body.style.backgroundColor = "magenta";
+  var x= document.getElementsByClassName("bleu"); 
+  var i;
+  for (i = 0; i < x.length; i++){
+  	x[i].style.color ="blue";
+  	x[i].style.fontFamily = "Papyrus";
+  }
+  var heading = document.getElementsByClassName('green')
+  var y;
+  for (y =0; y <heading.length; y ++){
+  	heading[y].style.color ="green";
+  	heading[y].style.fontFamily = "Comic Sans MS";
+  }
+  }
+ 
+
